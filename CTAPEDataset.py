a953e31681de9fd57fc85b01e1550cc25957ca3d
Gan4x4 <gan4x4@gmail.com>
2023-08-24 19:40:26 +0300

    Remove merge

--- CTAPEDataset.py
+++ CTAPEDataset.py
@@ -3,15 +3,16 @@ import pandas as pd
 import numpy as np
 from math import isnan
 
-SAMPLE_ID = ["Sample ID", "Sample #"]
+MATERIAL_ID = ["Sample ID", "Sample #"]
 WAVELENGTH = ["Wavelength (nm)", "Wavelength"]
+PSF_FILE = ["PSF File", "RELAB File", "File name", "Basename", "Basename (.txt)"]
 
 
 class CTAPEDataset(Dataset):
     wavelength = []
     sample_id = "Sample ID"
     # parts = []
-    items = {}
+    items = []
 
     def __init__(self, path_to_xlsx):
         # super().__init__()
@@ -23,12 +24,11 @@ class CTAPEDataset(Dataset):
             for i, p in enumerate(parts):
                 try:
                     self.parse(p)
-                #except AssertionError as e:
                 except Exception as e:
-                    print(f"part {i} skipped because of :v{e}" )
+                    print(f"part {i} skipped because of :v{e}")
                     continue
-            print(list(self.items.keys()))
-
+            print(self.classes())
+            # print(list(self.items.keys()))
 
     def split(self, df):
         parts = []
@@ -43,35 +43,27 @@ class CTAPEDataset(Dataset):
         return parts
 
     def parse(self, df):
-        keys = self.get_keys(df)
-        row = self.get_sample_id_row(df, keys)
-        wl_row_num = self.get_wavelen_row_num(df, keys)
-        wl = df.iloc[wl_row_num+1:, 0]
-        for i, s_id in enumerate(row.iloc[1:]):
-            s_id = str(s_id).strip()
+        keywords = self.get_keywords(df)
+        material_id_row = self.get_sample_id_row(df, keywords)
+        # psf_file_row = self.get_ps_file_row_num(keywords)
+        wl_row_num = self.get_wavelen_row_num(keywords)
+        wl = df.iloc[wl_row_num + 1:, 0]
+        for i, s_id in enumerate(material_id_row.iloc[1:]):
+            # psf_file_name = psf_file_row.iloc[1+i]
+            s_id = str(s_id).strip()  # + "_" + str(psf_file_name).strip()
             if len(s_id) and s_id != 'nan':
                 # assert not s_id in self.items, s_id
-                #s_id_str = self.find_keyphrase(keys.keys(),SAMPLE_ID)
-                values = df.iloc[wl_row_num+1:, i+1]
+                # s_id_str = self.find_keyphrase(keys.keys(),SAMPLE_ID)
+                values = df.iloc[wl_row_num + 1:, i + 1]
                 spectre = pd.concat([wl, values], axis=1)
                 spectre = spectre.dropna(how='all')
-                if s_id in self.items:
-                    x = np.concatenate([spectre.to_numpy(), self.items[s_id]], axis=0)
-                    self.items[s_id] = x
-                else:
-                    self.items[s_id] = spectre.to_numpy()
+                # if s_id in self.items:
+                #    x = np.concatenate([spectre.to_numpy(), self.items[s_id]], axis=0)
+                #    self.items[s_id] = x
+                # else:
+                self.items.append([s_id, spectre.to_numpy()])
 
-    def get_sample_id_row(self, df, keys):
-        s_id_str = self.find_keyphrase(keys.keys(),SAMPLE_ID)
-        n = keys[s_id_str]
-        x = df.iloc[n]
-        return x
-
-    def get_wavelen_row_num(self,df,keys):
-        wl_id_str = self.find_keyphrase(keys, WAVELENGTH)
-        return keys[wl_id_str]
-
-    def get_keys(self, df):
+    def get_keywords(self, df):
         out = {}
         # print(df.iloc[:20,0])
         for i, k in enumerate(df.iloc[:, 0]):
@@ -79,12 +71,28 @@ class CTAPEDataset(Dataset):
                 out[k] = i
             if k in WAVELENGTH:
                 break
-        assert self.find_keyphrase(out, SAMPLE_ID) is not None, f" Sample id({SAMPLE_ID}) not found in {out}"
-        assert self.find_keyphrase(out, WAVELENGTH) is not None, f" Wavelength({WAVELENGTH}) not found in {out}"
+        for kw in [MATERIAL_ID, WAVELENGTH]:
+            assert self.find_keyphrase(out, kw) is not None, f" Sample id({kw}) not found in {out}"
 
         for wl in df.iloc[i:, 0]:
             self.wavelength.append(wl)
         return out
+
+    def get_sample_id_row(self, df, keys):
+        s_id_str = self.find_keyphrase(keys.keys(), MATERIAL_ID)
+        n = keys[s_id_str]
+        x = df.iloc[n]
+        return x
+
+    def get_wavelen_row_num(self, keys):
+        wl_id_str = self.find_keyphrase(keys, WAVELENGTH)
+        return keys[wl_id_str]
+
+    def get_ps_file_row(self, df, keys):
+        ps_id_str = self.find_keyphrase(keys, PSF_FILE)
+        n = keys[ps_id_str]
+        x = df.iloc[n]
+        return x
 
     def find_keyphrase(self, arr, keyphrase_synonims):
         intersection = list(set(keyphrase_synonims) & set(arr))
@@ -93,13 +101,16 @@ class CTAPEDataset(Dataset):
         return intersection[0]
 
     def __len__(self):
-        return len(self.items.keys())
+        return len(self.items)
 
     def __getitem__(self, n):
-        keys = list(self.items.keys())
-        keys.sort()
-        key = keys[n]
-        a = self.items[key]
-        sorted = a[a[:, 0].argsort()]
-        #sorted = np.sort(arr,axis=0)
-        return key, sorted
+        s_id, a = self.items[n]
+        spectre = a[a[:, 0].argsort()]
+
+        return s_id, spectre
+
+    def classes(self):
+        x = set()
+        for s_id, _ in self.items:
+            x.add(s_id)
+        return x
